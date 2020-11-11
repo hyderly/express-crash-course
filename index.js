d75b66a0daa5f5cbe928c123965031aa5c56ad5c
haider ali <haidertiger10@gmail.com>
2020-11-11 03:51:48 -0800

    add

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const path = require("path");
+const exphbs = require("express-handlebars");
+
+const members = require("./Members");
 
 const logger = require("./middleware/logger");
 
@@ -8,17 +11,27 @@ const app = express();
 // initialize middleware
 // app.use(logger);
 
+// Handlebars Middleware
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
 // Body parser Middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
+// Render Handlebars
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Members App',
+        members,
+    })
+})
 
 // Set Static Folder
 app.use(express.static(path.join(__dirname, "public")));
 
 // Members API Routes
 app.use("/api/members", require("./routes/api/members"));
-
-
 
 
 const PORT = process.env.PORT || 5000;
--- routes/api/members.js
+++ routes/api/members.js
@@ -41,6 +41,39 @@ router.post('/', (req, res) => {
   else members.push(newMember);
 
   res.json(members);
+});
+
+
+// Update member
+router.put("/:id", (req, res) => {
+  const found = members.some((members) => members.id === +req.params.id);
+
+  if(found){
+    const newudMember = req.body;
+    members.forEach(member => {
+      if(member.id === +req.params.id){
+        member.name = newudMember.name ? newudMember.name : member.name;
+        member.email = newudMember.email ? newudMember.email : member.email;
+        member.role = newudMember.role ? newudMember.role : member.role;
+
+        res.json(members);
+      }
+    })
+  }else{
+    res.status(400).json(`User with id ${req.params.id} is not found`)
+  }
+});
+
+// Delete Member
+router.delete("/:id", (req, res) => {
+  const found = members.some((members) => members.id === +req.params.id);
+
+  if(found){
+    res.json({msg: 'User deleted', members: members.filter(member => member.id !== +req.params.id)})
+  }else{
+    res.status(400).json(`User with id ${req.params.id} not Found`);
+  }
 })
+
 
 module.exports = router;
